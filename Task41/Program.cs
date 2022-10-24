d7460d97b10e36d939cb48fd171981ff0b369318
SergyVR <rsv1177@gmail.com>
2022-10-24 17:55:56 +0300

    first commit

---
+++ Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+
+bool stop = false; 
+int number = 0; 
+int summ = 0; 
+
+while(!stop)
+{     Console.WriteLine("введите число");
+     number = int.Parse(Console.ReadLine());
+
+     if(number > 0)
+          summ++;
+Console.WriteLine("Продолжить вводить числа? да/нет");
+     string result = Console.ReadLine();
+    if(result == "да")
+       stop = false; 
+    else 
+       stop = true; 
+}
+Console.WriteLine($"Кол-во чисел, введеных пользователем, которые больше 0 = {summ}");
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1; y = k2 * x + b2
+
+Console.WriteLine("Введите число k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+if (k1 > k2 || k1 < k2 )
+{
+   double X = Math.Round((b2 - b1) / (k1 - k2)); 
+    double Y = Math.Round(k2 * X + b2); 
+   Console.WriteLine($"\nТочка пересечения двух прямых имеет координаты: ({X}; {Y})\n"); 
+}
+else
+{
+   Console.WriteLine("Прямые не пересекаются, они параллельны");   
+} 
+
